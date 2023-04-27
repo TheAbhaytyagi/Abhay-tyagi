a01cd5fa4f121cd7b30e26ae2445486fa7365387
Abhay tyagi <131832267+TheAbhaytyagi@users.noreply.github.com>
2023-04-28 02:55:41 +0530

    Add files via upload

---
+++ 2stProgram.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() 
+{
+
+int c,f;
+
+printf("value of f  = ");
+scanf("%d",&f);
+
+ c = (f-32)/9;
+
+ printf("value of f in c = %d",c);
+
+
+return 0 ;
+
+}
+
